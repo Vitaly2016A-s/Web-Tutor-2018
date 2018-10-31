5c6c5ba48f71dde43a611ba0a615361a4402e194
Vitaly A.92 <vitaly92a@gmail.com>
2018-11-01 01:03:51 +0300

    simple folder show
    removed from .gitignore and ready for a work

---
+++ simple/code.mu/1/script.js
@@ -0,0 +1,116 @@
+/* 01 */
+
+//var num = 3; alert(num);
+
+/* 02 */
+
+/*var a=10, b=1;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+*/
+
+/* 03 */
+
+/*
+var c=15, d=2;
+var result = c + d;
+alert(result); 
+*/
+
+/* 04 */
+
+//var a=10,b=2,c=5; alert(a+b+c);
+
+/* 05 */
+
+// var a=17,b=10;
+// var c = a - b;
+// var d = 7;
+// result = c + d; alert(result);
+
+/* 06 */
+
+/*
+var str = 'Привет, Мир!';
+alert(str);
+*/
+
+/* 07 */
+
+/*
+var str1="Привет, ", str2="Мир!";
+alert(str1+str2);
+
+*/
+/* 08 */
+
+/*var name="Vitaly";
+alert("Привет, " + name);
+*/
+
+/* 09 */
+
+/*
+var age=26;
+alert("Мне " + age + " лет!");
+*/
+
+/* 10 */
+/*
+var name = prompt("Ваше имя: ");
+alert(name);
+*/
+/* 11 */
+/*
+var num = prompt("Число: ");
+alert(num*num);
+*/
+/* 12 */
+/*
+var str = "abcde";
+alert(str[0]);
+alert(str[2]);
+alert(str[4]);
+*/
+/* 13 */
+/*
+var num = "12345";
+alert(num[0]*num[1]*num[2]*num[3]*num[4]);
+*/
+/* 14 */
+/*
+var hour = 60*60,
+		day = hour * 24,
+		month = day*30;
+*/
+
+/* 15 */
+/*
+var hour = 1;
+var minute = 25;
+var seconds = 15;
+alert(hour + ":" + minute + ":" + seconds)
+*/
+/* 16 */
+/*
+var num=15;
+alert(num*num);
+*/
+/* 17 */
+/*
+var num = 47;
+num += 7;
+num -= 18;
+num *= 10;
+num /= 15;
+alert(num);
+*/
+/* 18 */
+/*
+var num = 10;
+num++;
+num++;
+num--;
+alert(num);*/
---
+++ simple/code.mu/4/script.js
@@ -0,0 +1,8 @@
+/*
+* @Author: User
+* @Date:   2018-09-07 21:36:28
+* @Last Modified by:   User
+* @Last Modified time: 2018-09-07 21:38:05
+*/
+
+/* Глава 4*/
